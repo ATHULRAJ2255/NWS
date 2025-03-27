9e0a46242188c0980daf1a82f336140bd47cc946
ATHULRAJ2255 <152466316+ATHULRAJ2255@users.noreply.github.com>
2025-03-27 15:29:00 +0530

    Add files via upload

---
+++ oddoreven.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+even_odd(){
+echo "Enter the number"
+read a
+if [ $((a%2)) -eq 0 ]
+then 
+echo "The number is even"
+else
+echo "The number is odd"
+fi
+}
+even_odd
---
+++ reverse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+reverse(){
+echo "Enter the string"
+read a
+reverse=$(echo "$a"|rev)
+echo $reverse
+if [
+}
+reverse
+
+
